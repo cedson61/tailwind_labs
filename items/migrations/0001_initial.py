8361c85023d08730ea6de0c152dde05413d73599
Howard Edson <Howard.Edson@gmail.com>
2015-02-07 20:26:53 -0800

    removing bad migrations

--- items/migrations/0001_initial.py
+++ items/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,14 +16,16 @@ class Migration(migrations.Migration):
             name='Item',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField()),
                 ('created', models.DateTimeField(auto_now_add=True)),
-                ('last_updated', models.DateTimeField(auto_now=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('title', models.CharField(max_length=200)),
+                ('description', models.TextField(blank=True)),
                 ('is_active', models.BooleanField(default=True)),
-                ('url', models.URLField()),
+                ('url', models.URLField(blank=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'ordering': ['-modified'],
             },
             bases=(models.Model,),
         ),
