53f6e389b53765bfb2a451628b4bedfea8fb44df
Howard Edson <Howard.Edson@gmail.com>
2015-02-07 17:52:52 -0800

    config changes

--- tailwind_labs/settings.py
+++ tailwind_labs/settings.py
@@ -36,7 +36,7 @@ ADMINS = ('Howard Edson', 'howard.edson@gmail.com',)
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
@@ -112,10 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/howard/dev/django/tailwind_labs/static'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
+STATIC_ROOT = '/home/howard/dev/django/tailwind_labs/static/'
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
